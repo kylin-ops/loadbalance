600493b6b0a09e2a2cd13c1e4186bdc69b58b3c7
刘清 <liuqing2@shein.com>
2022-01-28 20:28:31 +0800

    fix emample

--- example/main.go
+++ example/main.go
@@ -10,15 +10,15 @@ import (
 
 func main(){
 	instances1 := []*balance.Instance{
-		{Host: "1.1.1.1", Port: 80, Scheme: "http"},
-		{Host: "1.1.1.1", Port: 81, Scheme: "https"},
-		{Host: "1.1.1.1", Port: 82, Scheme: "http"},
+		{Host: "1.1.1.1", Port: 80, Meta: map[string]string{"scheme": "http"}},
+		{Host: "1.1.1.1", Port: 81, Meta: map[string]string{"scheme": "http"}},
+		{Host: "1.1.1.1", Port: 82, Meta: map[string]string{"scheme": "http"}},
 	}
 
 	instances2 := []*balance.Instance{
-		{Host: "1.1.1.2", Port: 80, Scheme: "https"},
-		{Host: "1.1.1.2", Port: 81, Scheme: "http"},
-		{Host: "1.1.1.2", Port: 82, Scheme: "https"},
+		{Host: "1.1.1.2", Port: 80, Meta: map[string]string{"scheme": "http"}},
+		{Host: "1.1.1.2", Port: 81, Meta: map[string]string{"scheme": "http"}},
+		{Host: "1.1.1.2", Port: 82, Meta: map[string]string{"scheme": "http"}},
 	}
 	for {
 		log.Println(loadbalance.DoBalance(balance.BalanceTypeRoundRobin, instances1))
